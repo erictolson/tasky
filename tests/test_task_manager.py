bbd6f975290539fd963002800081f94723b093f4
Eric Tolson <91225947+erictolson@users.noreply.github.com>
2025-05-06 16:50:52 -0700

    simple test and github workflow

---
+++ tests/test_task_manager.py
@@ -0,0 +1,16 @@
+import unittest
+from task_manager import add_task, list_tasks, clear_tasks
+
+class TaskManagerTest(unittest.TestCase):
+
+    def setUp(self):
+        clear_tasks()
+
+    def test_add_task(self):
+        add_task("Test Task")
+        tasks = list_tasks()
+        self.assertEqual(len(tasks), 1)
+        self.assertEqual(tasks[0]["title"], "Test Task")
+
+if __name__ == '__main__':
+    unittest.main()
